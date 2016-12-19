af3a2d1dd178c94cd3fe71e215aac1ed8d159a91
nusbaoning <544124775@qq.com>
2016-12-19 22:35:45 +0800

    Period version
    Add PLRU and PLFU and pass basic tests
    Change the get data quality into new version using period type

--- mts_cache_algorithm.py
+++ mts_cache_algorithm.py
@@ -2,6 +2,184 @@ import sys
 import time
 import collections
 import operator 
+
+PERIODNUM = 10
+PERIODLEN = 10 ** 4
+
+
+class Period(object):
+    """docstring for Period"""
+
+
+    # O(1)
+    def __init__(self, size, alg, sleepStart = 51, sleepInterval = 30, throt = 1000):
+        self.period = -1
+        self.req = 0
+        self.nextUpdatePeriod = -1
+
+        self.sleepStart = sleepStart
+        self.sleepInterval = sleepInterval
+        self.alg = alg
+        self.throt = throt
+        self.size = size
+
+        self.ssd = alg(size)
+
+        self.hisDict = HistoryDict()
+
+        self.state = "warm"
+        
+        self.init_potential_dict()
+    # O(1)
+    def is_hit(self, blockID):
+        self.ssd.is_hit(blockID)
+
+    def get_top_n(self, num):
+        return self.ssd.get_top_n(num)
+
+    def init_potential_dict(self):
+        if self.alg == LRU:
+            self.potentialDict = self.alg(int(self.throt * 1.1))
+        elif self.alg == MT:
+            self.potentialDict = LFU(self.throt)
+        else: #PLFU
+            self.potentialDict = PLFU(PERIODLEN * PERIODNUM)
+
+    def access_data(self):
+        self.req += 1
+        if self.req >= PERIODLEN:
+            self.req = 0
+            self.period += 1
+    # warm up : dict set, average O(1)
+    # record : dict get & set, average O(1)
+    # period update : 
+    # pLRU O(throt)
+    # pLFU n=max(periodlen*periodnum,ssd size) O(n)
+    # MT n = (2*periodlen*periodnum), (max items in record dict)
+    # O(n) + O(sizelogsize)
+    def update_cache(self, blockID):
+        # warm up state
+        if self.state == "warm":
+            self.ssd.update_cache(blockID)
+            if self.ssd.update >= self.ssd.size:
+                self.state = "learn"
+                self.period = 1
+                self.nextUpdatePeriod = 1
+                self.periodRecord = True
+            return
+
+        self.req += 1
+        # check whether record is needed        
+        if self.periodRecord and self.alg != LRU:
+            self.hisDict.access_data(blockID, self.period)
+        if blockID not in self.ssd.ssd:
+            self.potentialDict.update_cache(blockID)
+        # check whether period finishes
+        if self.req >= PERIODLEN:                       
+            # update needed?
+            if self.period == self.nextUpdatePeriod: 
+                # print("test update", self.period, self.ssd.update, len(self.potentialDict.ssd))  
+                if self.alg == LRU:
+                    self.ssd.update_cache_k(self.throt, self.potentialDict)
+                # elif alg == MT:
+                #     self.hisDict = self.ssd.update_cache_k(self.throt, 
+                #         self.potentialDict, self.hisDict, self.period)
+                else: #PLFU
+                    self.ssd.update_cache_k(self.throt, self.potentialDict, self.hisDict, self.period)
+                
+                self.init_potential_dict()
+
+                # compute next update period
+                # sleep state
+                if self.period+1 >= self.sleepStart:
+                    self.state = "sleep"
+                    self.nextUpdatePeriod = self.period + self.sleepInterval
+                    # check whether the period should be recorded
+
+                    # if period = 1 and next update period = 10, 
+                    # when 10th period finishes, ssd will be updated
+                    # 1st to 10th period should be recorded
+                    # when nextUpdatePeriod = 11, fails
+        
+                    if self.nextUpdatePeriod - self.period <= PERIODNUM:
+                        # print(self.period, "record period")
+                        self.periodRecord = True
+                    # no need to record
+                    # empty the history dict
+                    else:
+                        # print(self.period, "not record period")
+                        self.periodRecord = False
+                        self.hisDict = HistoryDict()
+                # learn state
+                else:
+                    self.nextUpdatePeriod += 1
+                    self.periodRecord = True
+
+            # check history record
+            else:
+                if self.nextUpdatePeriod - self.period <= PERIODNUM:
+                    self.periodRecord = True        
+            self.req = 0 
+            self.period += 1
+
+
+    def delete_cache(self, blockID):
+        self.ssd.delete_cache(blockID)
+        self.potentialDict.delete_cache(blockID)
+
+    def print_sample(self):
+        self.ssd.print_sample()
+        self.hit = self.ssd.hit
+
+
+        
+class HistoryDict(object):    
+    """docstring for HistoryDict"""
+    def __init__(self):
+        self.d = {}
+    def access_data(self, blockID, period):
+        if blockID in self.d:
+            (pointer, accL, lastAccT) = self.d[blockID]
+            if lastAccT == period:
+                accL[pointer] += 1
+            else:
+                if period - lastAccT >= PERIODNUM:
+                    accL = [0]*PERIODNUM
+                    accL[0] = 1
+                    self.d[blockID] = (0, accL, period)
+                else:
+                    i = 0
+                    for p in range(lastAccT + 1, period):
+                        i += 1
+                        accL[(pointer + p) % PERIODNUM] = 0
+                    pointer = (pointer + i + 1) % PERIODNUM
+                    accL[pointer] = 1
+                    self.d[blockID] = (pointer, accL, period)
+        else:
+            accL = [0]*PERIODNUM
+            accL[0] = 1
+            self.d[blockID] = (0, accL, period)
+
+    def get_history_data(self, blockID, period):
+        if blockID in self.d:
+            (pointer, accL, lastAccT) = self.d[blockID]
+            if lastAccT == period:
+                l = [0] * PERIODNUM
+                for i in range(0,PERIODNUM):
+                    l[i] = accL[(pointer + i) % PERIODNUM]
+                return l
+            elif period - lastAccT >= PERIODNUM:
+                del self.d[blockID]
+                return None
+            else:
+                l = [0] * PERIODNUM
+                for i in range(max(0,period-9), lastAccT+1):
+                    # print("test", i, period - i, ((pointer + (PERIODNUM - lastAccT + i)) % PERIODNUM))
+                    l[period - i] = accL[(pointer + (PERIODNUM - lastAccT + i)) % PERIODNUM]
+                return l                
+        else:
+            return None
+
 class CacheAlgorithm(object):
 
     """docstring for CacheAlgorithm"""
@@ -15,7 +193,7 @@ class CacheAlgorithm(object):
     def delete_cache(self, key):
         pass
     '''
-    get the top n block ids from ssd
+    get the top n blockids from ssd
     '''
     def get_top_n(self, number):
         pass
@@ -188,10 +366,30 @@ class LRU(CacheAlgorithm):
 
     def print_sample(self):
         print("print LRU ssd")
-        for key in self.ssd.keys():
-            print(key, end=",")
+        if len(self.ssd) <= 100:            
+            node = self.head
+            for i in range(len(self.ssd)):
+                print(node.key, end=",")
+                node = node.next        
+            print()
         print("hit", self.hit)
         print("write", self.update)
+
+    def update_cache_k(self, throt, potentialDict):
+
+        node = potentialDict.head
+        # print("potential dict")
+        # print(len(potentialDict.ssd))
+        # potentialDict.print_sample()
+        throt = min(throt, len(potentialDict.ssd))
+        for i in range(1, throt):
+            node = node.next
+        for i in range(0, throt):
+            self.update_cache(node.key)
+            # print(node.key)
+            node = node.prev
+
+
 
 class LFU(CacheAlgorithm):
     """docstring for LFU"""
@@ -207,6 +405,7 @@ class LFU(CacheAlgorithm):
             super().is_hit()
             return True
         return False
+
     def update_cache(self, key):
         if key in self.ssd:            
             if self.ssd[key] == self.minreq:
@@ -246,7 +445,7 @@ class LFU(CacheAlgorithm):
                 break
             self.victims.append(key)
     
-    def delete_cache(self,key):
+    def delete_cache(self, key):
         if key not in self.ssd:
             return
         del(self.ssd[key])
@@ -258,26 +457,181 @@ class LFU(CacheAlgorithm):
 
     def print_sample(self):
         print("print LFU ssd")
-        for key in self.ssd.keys():
-            print(key, self.ssd[key])
+        if len(self.ssd) <= 100:
+            for key in self.ssd.keys():
+                print(key, self.ssd[key])
         print("hit", self.hit)
         print("write", self.update)
 
 
+class PLFU(CacheAlgorithm):
+    """docstring for PLFU"""
+    def __init__(self, size):
+        super().__init__()
+        self.size = size
+        self.ssd = {}
+
+    def is_hit(self, key):
+        if key in self.ssd:
+            super().is_hit()
+            return True
+        return False   
+
+    # attention : only called in warm state
+    # no check of size
+    def update_cache(self, blockID):
+        if blockID not in self.ssd:            
+            self.ssd[blockID] = 1
+            super().update_cache()
+
+    def delete_cache(self, key):
+        if key not in self.ssd:
+            return
+        del(self.ssd[key])
+
+    def update_cache_k(self, throt, potentialDict, hisDict, period):
+        # get evict items from ssd and do eviction
+        if self.size - len(self.ssd) < throt:
+            evictNum = min(throt, len(potentialDict.ssd)) - (self.size - len(self.ssd))
+            # print("evictnum", evictNum)
+            # calculate requests of ssd data and record to a dict based on request numbers
+            d = {}
+            for blockID in self.ssd.keys():
+                rl = hisDict.get_history_data(blockID, period)
+                if rl == None:
+                    s = 0
+                else:
+                    s = sum(rl)
+                if s not in d:
+                    d[s] = [blockID]
+                else:
+                    d[s].append(blockID)
+                # print("test hisDict, return list & sum", blockID, rl, s)
+
+            # directly delete evictNum data with least requests
+            keys = list(d.keys())
+            keys.sort()
+            
+
+            for key in keys:
+                for i in range(0, len(d[key])):
+                    if evictNum == 0:
+                        break
+                    del(self.ssd[d[key][i]])
+                    evictNum -= 1
+                if evictNum == 0:
+                    break 
+
+        # print("potential dict", potentialDict.ssd.keys())
+        # calculate sum for potential data
+        d = {}
+        for blockID in potentialDict.ssd.keys():
+            l = hisDict.get_history_data(blockID, period)
+            if l == None:
+                pass
+            else:
+                s = sum(l)
+                if s not in d:
+                    d[s] = [blockID]
+                else:
+                    d[s].append(blockID)
+
+        # get top throt data from potential list and add to ssd
+        # if period == 80:
+        #     print(throt)
+
+
+        keys = list(d.keys())
+        keys.sort(reverse=True)
+        # if period == 80:
+        #     print(len(keys))
+        for key in keys:
+            # if period == 80:
+            #     print(key)
+            for i in range(0, len(d[key])):
+                if throt == 0:
+                    break
+                # if period == 80:
+                #     print("before update", self.update, len(self.ssd))
+                self.update_cache(d[key][i])
+                # if period == 80:
+                #     print("after update", self.update, len(self.ssd))
+                throt -= 1
+            if throt == 0:
+                break 
+
+    def print_sample(self):
+        print("print PLFU ssd")
+        if len(self.ssd) <= 100:
+            print(self.ssd.keys())  
+        print("hit", self.hit)
+        print("write", self.update)
+
+    # no order in PLFU
+    # will only return total ssd
+    def get_top_n(self, num):
+        print("this function")
+        # print(len(list(self.ssd.keys())[0:num]))
+        result = list(self.ssd.keys())
+        return result
+        
+class MT(CacheAlgorithm):
+    """docstring for MT"""
+    def __init__(self, arg):
+        super(MT, self).__init__()
+        self.arg = arg
+        
 
 
 
+def test_hisdict_acc():
+    mydict = HistoryDict()
+    mydict.access_data(0, 1)
+    print(mydict.d)
+    mydict.access_data(0, 1)
+    print(mydict.d)
+    mydict.access_data(0, 2)
+    print(mydict.d)
+    mydict.access_data(0, 5)
+    print(mydict.d)
+    mydict.access_data(0, 16)
+    print(mydict.d)
+    mydict.access_data(1, 5)
+    print(mydict.d)
 
 
+def test_plru():
+    ssd = Period(alg = LRU, size = 10, throt = 5, sleepStart = 3, sleepInterval = 2)
+    period = 1
+    for i in range(1,100):
+        ssd.update_cache(i)
+        if period < ssd.period:
+            period = ssd.period
+            print(i, ":")   
+            ssd.ssd.print_sample()
+            print("state", ssd.state)
 
-# ssd = LFU(5)
-# ssd.print_sample()
-# for i in range(1, 5):
-#     print(i)
-#     for j in range(1,i+1):  
-#         ssd.update_cache(i)
-#         ssd.is_hit(i)
-#     ssd.print_sample()
+def test_get_history_data():
+    mydict = HistoryDict()
+    for i in range(1,10):
+        for j in range(0,i):
+            mydict.access_data(1, i)
+    print(mydict.d[1])
+    print(mydict.get_history_data(1, 15))
+    print(mydict.get_history_data(2,15))
+    print(mydict.get_history_data(1,20))
+    print(mydict.d)
 
-
+def test_plfu():
+    
+    ssd = Period(alg = PLFU, size = 10, throt = 5, sleepStart = 3, sleepInterval = 2)
+    period = 0
+    for i in range(0,50):
+        for j in range(0, (i % 10 + 1)):
+            ssd.update_cache(i)  
+        if period < ssd.period:      
+            period = ssd.period
+            print(i, ":")   
+            ssd.ssd.print_sample()
+            print("state", ssd.state)
 
--- mts_data_quality.py
+++ mts_data_quality.py
@@ -3,23 +3,23 @@ import operator
 import time
 import sys
 
-TIME_WINDOW = 10 ** 4
 
+TIME_WINDOW = 10**4
 readReq = 0
 reqdict = {}
-period = 0
-sign = False
 ssd = None
+period = 0
 logFile = open("log", "a")
 s = time.time()
+ucln = 0
 
 def init():
-    global readReq, reqdict, period, sign, ssd
+    global readReq, reqdict, ssd, period
+    period = 0
     readReq = 0
     reqdict = {}
-    period = 0
-    sign = False
     ssd = None
+    ucln = 0
 
 def update_req_dict(key):
     global reqdict
@@ -40,15 +40,57 @@ def load_file(filename, start, end):
     lines = fin.readlines()
     lineNum = len(lines)
     i = 0
+    percent = 0
     for line in lines:
         i += 1
-        if readReq % TIME_WINDOW == 0 and (not sign):            
-            period+=1
-            if period % 10 == 0:
+
+        if readReq % TIME_WINDOW == 0:     
+            # print(readReq, period, ssd.period)             
+            presentPercent = int(100*i/lineNum)
+            if presentPercent >= percent+5:
                 e = time.time()
-                print(period, " period finished", int(100*i/lineNum), "%", (e-s), "time consumed")
+                print(presentPercent, "%", (e-s), "s consumed")
                 s = e
-            sign = True
+                percent = presentPercent
+
+        if line[-1] == "\n":
+            line = line[0:-1]
+        items = line.split(' ')
+        reqtype = int(items[0])
+        block = int(items[2])
+        if reqtype == 0:
+            readReq += 1            
+            period = ssd.period
+            if period == -1:
+                ssd.update_cache(block)                          
+            elif period in range(start, end):
+                ssd.update_cache(block)
+            elif period >= end:
+                ssd.access_data()   
+            update_req_dict(block)
+        else:
+            if period in range(start, end):
+                ssd.delete_cache(block)
+    fin.close()
+
+def simulator(filename):
+    global ssd, readReq, s
+    # try:
+    fin = open(filename, 'r', encoding='utf-8', errors='ignore')
+    lines = fin.readlines()
+    lineNum = len(lines)
+    i = 0
+    percent = 0
+    for line in lines:
+        i += 1
+        if readReq % TIME_WINDOW == 0:                  
+            presentPercent = int(100*i/lineNum)
+            if presentPercent >= percent+5:
+                e = time.time()
+                print(presentPercent, "%", (e-s), "s consumed")
+                s = e
+                percent = presentPercent
+            
         if line[-1] == "\n":
             line = line[0:-1]
         items = line.split(' ')
@@ -56,86 +98,144 @@ def load_file(filename, start, end):
         block = int(items[2])
         if reqtype == 0:
             readReq += 1
-            sign = False
-            if period in range(start, end+1):
-                ssd.update_cache(block)
-            update_req_dict(block)
+            ssd.is_hit(block)
+            ssd.update_cache(block)
         else:
-            if period in range(start, end+1):
-                ssd.delete_cache(block)
+            ssd.delete_cache(block)
     fin.close()
 
-'''
-return a dict [(key, rank)]
-scope = "global" / (start, end)
-test finished
-'''
-def cal_rank_dict(scope):
-    rd = {}
-    if scope == "global":
-        for key in reqdict.keys():
-            l = reqdict[key]
-            rd[key] = sum(i for _, i in l)
-    else:
-        start, end = scope
-        for key in reqdict.keys():
-            l = reqdict[key]
-            rd[key] = 0
-            for item in l:
-                per,req = item
-                if per >= end:
-                    break
-                if per < start:
-                    continue
-                rd[key] += req
-            # if rd[key] == 0:
-            #     del rd[key]
-    # print(rd)
-    l = list(rd.items())        
-    l.sort(key=operator.itemgetter(1), reverse=True)
-    # print("sorted line", l, file=sys.stdout)
-    # print("line76", l)
-    i = 0
-    pre = -1
-    # the version for a list that the number of a specific value is counted
-    # j = 1
-    # for key,req in l:        
-    #     if pre == req:
-    #         j += 1
-    #     else:
-    #         print(req, i)
-    #         pre = req
-    #         i += j
-    #         j = 1
-    #     rd[key] = i
-    # pure req rank version
-    
-    for key,req in l:        
-        if pre == req:
-            pass
-        else:
-            pre = req
-            i += 1
-        rd[key] = i
-    dq = [0]*10
-    for key,_ in l:                
-        dq[int(10*rd[key]/(i+1))] += 1
-    print(dq)
-    print(dq, file=logFile)
-    # print("max rank = ", i)
-    # print("max index = ", int(10*i/len(rd)))
-    # print("rankdict", rd, file=sys.stdout)
-    return (rd, i)
 
-def get_data_quality(number, scope):
-    l = ssd.get_top_n(number)
-    rd,maxRank = cal_rank_dict(scope)
-    dq = [0]*10
+# return the request of block in [start, end)
+def get_req(block, start, end):
+    if block not in reqdict:
+        return 0
+    l = reqdict[block]    
+    r = 0
     for item in l:
-        rank = rd[item]
-        dq[int(10*rank/(maxRank+1))] += 1
-    print(scope, dq)
-    print(scope, dq, file=logFile)
+        per,req = item
+        if per >= end:
+            break
+        if per < start:
+            continue
+        r += req
+    return r
+
+
+# original version of 10 windows and get data quality
+# '''
+# return a dict [(key, rank)]
+# scope = "global" / (start, end)
+# test finished
+# '''
+# def cal_rank_dict(scope):
+#     rd = {}
+#     if scope == "global":
+#         for key in reqdict.keys():
+#             l = reqdict[key]
+#             rd[key] = sum(i for _, i in l)
+#     else:
+#         start, end = scope
+#         for key in reqdict.keys():
+#             l = reqdict[key]
+#             rd[key] = 0
+#             for item in l:
+#                 per,req = item
+#                 if per >= end:
+#                     break
+#                 if per < start:
+#                     continue
+#                 rd[key] += req
+#             # if rd[key] == 0:
+#             #     del rd[key]
+#     # print(rd)
+#     l = list(rd.items())        
+#     l.sort(key=operator.itemgetter(1), reverse=True)
+#     # print("sorted line", l, file=sys.stdout)
+#     # print("line76", l)
+#     i = 0
+#     pre = -1
+#     # the version for a list that the number of a specific value is counted
+#     # j = 1
+#     # for key,req in l:        
+#     #     if pre == req:
+#     #         j += 1
+#     #     else:
+#     #         print(req, i)
+#     #         pre = req
+#     #         i += j
+#     #         j = 1
+#     #     rd[key] = i
+#     # pure req rank version
+    
+#     for key,req in l:        
+#         if pre == req:
+#             pass
+#         else:
+#             pre = req
+#             i += 1
+#         rd[key] = i
+#     dq = [0]*10
+#     for key,_ in l:                
+#         dq[int(10*rd[key]/(i+1))] += 1
+#     print(dq)
+#     print(dq, file=logFile)
+#     # print("max rank = ", i)
+#     # print("max index = ", int(10*i/len(rd)))
+#     # print("rankdict", rd, file=sys.stdout)
+#     return (rd, i)
+
+# def get_data_quality(number, scope):
+#     l = ssd.get_top_n(number)
+#     rd,maxRank = cal_rank_dict(scope)
+#     dq = [0]*10
+#     for item in l:
+#         rank = rd[item]
+#         dq[int(10*rank/(maxRank+1))] += 1
+#     print(scope, dq)
+#     print(scope, dq, file=logFile)
+
+def get_ucln():
+    global ucln
+    ucln = len(reqdict)
+    print("ucln", ucln)
+
+def get_data_quality(start, interval):
+    end = start + interval
+    while end < period:
+        print(start, end)
+        cal_data(start, end)
+        start = end
+        end += interval
+
+def cal_data(start, end):
+    l = []
+    d = {}
+    for block in reqdict.keys():
+        req = get_req(block, start, end)
+        if req > 0:
+            d[block] = req
+    l = list(d.items())
+    l.sort(key=operator.itemgetter(1), reverse=True)
+    loc = min(int(0.01*ucln), len(l)-1)
+    print("1%", loc, l[loc])
+    _, req1 = l[loc]
+    loc = min(int(0.1*ucln), len(l)-1)
+    print("10%", loc, l[loc])
+    _, req10 = l[loc]
+    print("medium", len(l))
+    ssdL = ssd.get_top_n(ssd.size)
+    print(len(ssdL))
+    resultL = [0]*4
+    for block in ssdL:
+        if block not in d:
+            resultL[-1] += 1
+        elif d[block] >= req1:
+            resultL[0] += 1
+        elif d[block] >= req10:
+            resultL[1] += 1
+        else:
+            resultL[2] += 1
+    print(resultL)
 
 def generic_test_trace():
     trace = open("trace.req", "w")
@@ -149,22 +249,39 @@ def generic_test_trace():
             print("0 0", j, file=trace)
     trace.close()
 
-print("version 1.2")
-algL = [mts_cache_algorithm.LRU, mts_cache_algorithm.LFU]
-partialL = [10, 30, 100]
-for alg in algL:
+print("version 2.1.1")
+periodSign = [True] * 2
+algL = [mts_cache_algorithm.PLFU, mts_cache_algorithm.LRU]
+for i in range(0, len(algL)):
+    start = time.time()
     init()
-    ssd = alg(10000)
+    ssdSize = 10 ** 4
+    if periodSign[i]:
+        ssd = mts_cache_algorithm.Period(size = ssdSize, alg = algL[i])
+    else:
+        ssd = algL[i](ssdSize)
+    print(ssd.size)
     load_file("D:/Chai/MTS/data/usr_0.csv.req", 1, 11)
-    print(alg, file = logFile)
-    e = time.time()
-    print("load file:", e - s, "s comsued")
-    # get_data_quality(5000, "global")
-    # s = e
-    # e = time.time()
-    # print("global data quality:", e-s, "s comsued")
-    for parLen in partialL:
-        get_data_quality(5000, (12, 12+parLen))
-    
-logFile.close()
+    end = time.time()
+    print("load file consumed", end-start, "s")
+    get_ucln()
+    print("period is", period)
+    get_data_quality(11, 30)
+
+# periodSign = [True] * 2
+# algL = [mts_cache_algorithm.PLFU, mts_cache_algorithm.LRU]
+# for i in range(0, len(algL)):
+#     start = time.time()
+#     init()
+#     ssdSize = 10 ** 5
+#     if periodSign[i]:
+#         ssd = mts_cache_algorithm.Period(size = ssdSize, alg = algL[i])
+#     else:
+#         ssd = algL[i](ssdSize)
+#     simulator("D:/Chai/MTS/data/usr_0.csv.req")
+#     ssd.print_sample()
+#     print(100.0 * ssd.hit / readReq, "%")
+#     end = time.time()
+#     print("consumed", end-start, "s")
+
 
