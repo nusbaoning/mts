4d177cb28abfee827ce45a9d058d02e90bed564e
nusbaoning <544124775@qq.com>
2017-09-22 18:11:36 +0800

    Add files via upload
    创建文件

---
+++ parse_fb.py
@@ -0,0 +1,145 @@
+import sys
+import os
+import time
+
+
+
+def handle_fb_file(filename):    
+    infile = open(filename, 'r', encoding='utf-8', errors='ignore')
+    outname = filename +'.req'
+    outfile = open(outname, 'w')
+    
+    
+    
+    allLines = fin.readlines()    
+    for line in allLines:
+        # fop.write(line)
+        i += 1
+        
+        # message = line[LINE_LENGTH:]
+        # line = line[:LINE_LENGTH]       
+        items = line.strip().split()
+        # items from 0 to 6 are : 0 major,minor number; 1 #cpu; 2 #sequence;
+        # 3 timestamp; 4 pid; 5 action; 6 R/W
+
+        # the end part of the trace
+        if(len(items)<7):
+            # print("error", line)          
+            # print(items)
+            # print(i)
+            break
+
+        # if i >= 40:
+        #     return
+        action = items[5]
+        rw = items[6]
+        message = line[line.find(items[7], LINE_LENGTH):]
+
+        # print("items=", items, "action=", action, "rw=", rw)
+        
+
+        if action != "Q":
+            continue
+
+
+        j += 1
+        # if rw == "N" or action in IGNORE_CASES:
+        #     if "+" in message:
+        #         e1 += 1
+        #         l1.append(i)
+        #     continue
+
+        if "+" not in message:
+            e2 += 1
+            l2.append(i)
+            continue
+
+        # rwset.add(rw)
+
+        blocks = message.strip().split()
+        b_st = int(blocks[0])
+        nrb = int(blocks[2])
+        blkey = (b_st, nrb, rw)
+        # print(i, nrb, blocks)
+        ios = ios + int(nrb)
+
+
+log = "/home/bn/data/metadata.csv"
+logFile = open(log, "a")
+
+def handle_csv(fileid, filename):
+    block_size = 4096
+    flag=False
+    readcount=0
+    writecount=0   
+    readsize = 0
+    writesize = 0 
+    outname = filename +'.req'
+    count = 0
+    totalDict = {}
+    readDict = {}
+    writeDict = {}
+    lba=[[sys.maxsize,0], [sys.maxsize,0], [sys.maxsize,0]]
+    infile = open(filename, 'r', encoding='utf-8', errors='ignore')
+    outfile = open(outname, 'w')
+
+    for line in infile.readlines():
+        count += 1
+        line = line.strip().split(',')
+        block_id = int((float(line[4]))/block_size)
+        block_end = int((float(line[4])+float(line[5])-1)/block_size)
+        if count % 100000 == 0:
+            print(count)
+        if line[3]=='Write':
+            rw = 1
+            writecount += 1
+            writesize+=block_end-block_id+1
+        elif line[3]=='Read':
+            rw = 0
+            readcount+=1
+            readsize+=block_end-block_id+1
+        else:
+            rw = 2
+        if lba[2][0] > block_id:
+            lba[2][0] = block_id
+        if lba[2][1] < block_end:
+            lba[2][1] = block_end
+        if lba[rw][0] > block_id:
+            lba[rw][0] = block_id
+        if lba[rw][1] < block_end:
+            lba[rw][1] = block_end
+        for i in range(block_id,block_end+1):
+            print('{0} {1} {2}'.format(rw,line[2],i),file=outfile)
+            totalDict[i] = True
+            
+            if rw == 0:
+                readDict[i] = True
+            elif rw == 1:
+                writeDict[i] = True
+
+    print("read write", readcount, writecount, readcount/writecount, readsize, writesize, readsize/writesize, sep=',')
+    print("ucln", len(totalDict), len(readDict), len(writeDict), 
+        lba[2][1]-lba[2][0]+1, lba[0][1]-lba[0][0]+1, lba[1][1]-lba[1][0]+1, sep=',')
+    print(fileid, readcount+writecount, readcount, writecount, round(readcount/writecount, 2), 
+        readsize+writesize, readsize, writesize, round(readsize/writesize, 2), 
+        len(totalDict), len(readDict), len(writeDict), 
+        lba[2][1]-lba[2][0]+1, lba[0][1]-lba[0][0]+1, lba[1][1]-lba[1][0]+1, sep=',', file=logFile)
+    infile.close()
+    outfile.close()
+
+l = ["rsrch_0", "src2_0", "stg_0", "ts_0", "wdev_0", "web_0"]
+for i in l:
+    print(i)
+    start = time.clock()
+    handle_csv(i, "/mnt/raid5/trace/MS-Cambridge/" + i + ".csv")            
+    end = time.clock()
+    print("consumed ", end-start, "s")
+logFile.close()                   
+                 
+  
+
+
+
+
+
+
